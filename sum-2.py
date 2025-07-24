a0c40e6c26f0989d667ace6fa4b3982733e659d9
Yarramsetti Anusha <anushayarramsetti@gmail.com>
2025-07-24 11:45:34 +0530

    intit commit

---
+++ sum-2.py
@@ -0,0 +1,11 @@
+# FIND SECOND LARGEST NUMBER
+n=int(input())
+arr=list(map(int,(input().split())))
+first=second=float('-inf')
+for num in arr:
+    if num>first:
+        second=first
+        first=num
+    elif first>num>second:
+        second=num
+print(second)
